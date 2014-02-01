aefbb212551cfe17e8fc80dde40ad318196d6833
PikaDotus <loganh@pobox.com>
2014-01-31 17:47:07 -0800

    Initial Commit

---
+++ OpenCV Testing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCV Testing
+//
+//  Created by Logan Howard on 1/31/14.
+//  Copyright (c) 2014 Logan Howard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
