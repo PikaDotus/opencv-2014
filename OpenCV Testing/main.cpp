9950bee842c4665ce4acc8b30806386ba87e6b8d
PikaDotus <loganh@pobox.com>
2014-02-06 20:16:13 -0800

    still working; change approach

--- OpenCV Testing/main.cpp
+++ OpenCV Testing/main.cpp
@@ -24,6 +24,8 @@ void close(const Mat src, Mat dst, Mat element)
 	erode (src, dst, element);
 }
 
+float hueLow = 200, satLow = 40, valLow = 20, hueHigh = 245, satHigh = 80, valHigh = 65;
+
 Mat process(Mat &img)
 {
     // convert to HSV
@@ -39,7 +41,10 @@ Mat process(Mat &img)
     // S: (0-100, 0-255)
     // V: (0-100, 0-255)
     float kH = 0.5, kS = 2.55, kV = 2.55;
-    inRange(hsv, Scalar(kH * 200, kS * 40, kV * 20), Scalar(kH * 245, kS * 80, kV * 65), mask);
+    inRange(hsv,
+            Scalar(kH * hueLow, kS * satLow, kV * valLow),
+            Scalar(kH * hueHigh, kS * satHigh, kV * valLow),
+            mask);
 
     Point anchor1(10, 10), anchor2(5, 5);
     Size size1(21, 21), size2(11, 11);
@@ -52,31 +57,68 @@ Mat process(Mat &img)
     Mat hough_in(size, CV_8U);
     mask.copyTo(hough_in);
 
-//    GaussianBlur(hough_in, hough_in, Size(15, 15), 0, 0);
-//    
-//    Mat circles(size.height, size.width, CV_8UC1);
-//    HoughCircles(hough_in, circles, CV_HOUGH_GRADIENT, 4, size.height/10, 100, 40, 0, 0);
-//  
-//    for (int i = 0; i < circles.total(); ++i) {
-//        Point2f p(circles.at<Point2f>(i, 0));
-//        Point center(p.x, p.y);
-//        Scalar val(mask.at<Scalar>(center.y, center.x));
+    GaussianBlur(hough_in, hough_in, Size(15, 15), 2, 2); // last 2 were 0, 0
+    
+//    vector<Vec3f> circles;
+//    HoughCircles(hough_in, circles, CV_HOUGH_GRADIENT, 4, size.height/10, 100, 140, 20, 0);
+    
+    vector<vector<Point>> contours;
+    findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+    
+    int contourNum = -1;
+    double area = 0;
+    
+    for (int i = 0; i < contours.size(); ++i) {
+        double a = contourArea(contours[i]);
+        if (a > area) contourNum = i;
+    }
+    
+    if (contourNum == -1) return mask;
+    
+    vector<Point> masterContour(contours[contourNum]);
+    polylines(mask, masterContour, true, Scalar(255/2));
+    
+    Point2f centerEnclosing;
+    float radiusEnclosing = 0;
+    minEnclosingCircle(Mat(masterContour), centerEnclosing, radiusEnclosing);
+    
+    circle(mask, centerEnclosing, round(radiusEnclosing), 130);
+    
+//    for (int i = 0; i < circles.size(); ++i) {
+//        try {
+//            float x = circles[i][0], y = circles[i][1];
+//            if (x < 0 || y < 0) continue;
+//            if (x > mask.cols || y > mask.rows) continue;
+//            Scalar val(mask.at<Scalar>(x, y));
 //
-//        // note: whenever this next part fails, it jumps the memory up by about 30MB. perhaps this is related to memory climbs? memory stays constant when this whole for loop is commented.
-//        if (val.val[0] < 1) continue;
-//        
-//        circle(img, center, 3, CV_RGB(0, 255, 0));
-//        //circle(img, center, roundf(p[2]), CV_RGB(255, 0, 0));
-//        circle(mask, center, 3, CV_RGB(0, 255, 0));
-//        //circle(mask, center, roundf(p[2]), CV_RGB(255, 0, 0));
+//            if (val.val[0] < 1) continue;
+//            
+//            // drawing out of range on image – drawing off image therefore fails
+//            // TRY USING center of gravity to focus more on the right circle
+//            // also: tweak ALL parameters of houghCircles to find optimal
+//            Point2f center(x, y);
+//            
+//            circle(img, center, 3, CV_RGB(0, 255, 0));
+//            circle(img, center, circles[i][2], CV_RGB(255, 0, 0));
+//            circle(mask, center, 3, CV_RGB(0, 255, 0));
+//            circle(mask, center, circles[i][3], CV_RGB(255, 0, 0));
+//        } catch (int e) {
+//            std::cout << "Exception occured: " << e << std::endl;
+//        }
 //    }
     
-    return hough_in;
+    return mask;
 }
 
 void test_live()
 {
     VideoCapture cam(0);
+//    char    hueLowSlide[50],    satLowSlide[50],    valLowSlide[50],
+//            hueHighSlide[50],   satHighSlide[50],   valHighSlide[50];
+//    const int hueSlideMax = 180, satSlideMax = 255, valSlideMax = 255;
+//    double hueSlide, satSlide, valSlide;
+    
+    
 	for (;;) {
         Mat img;
         cam.read(img);
@@ -86,7 +128,9 @@ void test_live()
         flip(out, out, 1);
         
         //imshow("img", img);
-        imshow("out", out);
+        imshow("out", out); // error here
+        
+        //createTrackbar(hueLowSlide, "hue low", <#int *value#>, <#int count#>)
         
 		if (waitKey(10) == 27)
 			return;
