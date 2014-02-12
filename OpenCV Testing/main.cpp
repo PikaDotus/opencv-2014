2917572f31cdaf8971f27897f4b05509d47fb2d9
PikaDotus <loganh@pobox.com>
2014-02-11 21:44:54 -0800

    add bumper tracking; going to add bumper distance / orientation thingy

--- OpenCV Testing/main.cpp
+++ OpenCV Testing/main.cpp
@@ -32,6 +32,7 @@ const float kH = 0.5, kS = 2.55, kV = 2.55;
 
 void detectHot(Mat &img)
 {
+    // bounding values
     const float   hueLow = 160,   satLow = 25,    valLow = 90,
     hueHigh = 190,  satHigh = 75,   valHigh = 100;
     
@@ -53,28 +54,26 @@ void detectHot(Mat &img)
     vector<vector<Point>> contours;
     findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
     
-    int contourNum = -1;
+    // per contour...
     for (int i = 0; i < contours.size(); ++i) {
+        // draw it
         vector<Point> a = contours[i];
         polylines(img, a, false, Scalar(0, 0, 255), 1, CV_AA);
         
         RotatedRect rect(minAreaRect(a));
         
+        // draw the rotated rectangle enclosing it
         Point2f vertices[4];
         rect.points(vertices);
         for (int k = 0; k < 4; ++k) {
             line(img, vertices[k], vertices[(k + 1) % 4], Scalar(0, 255, 255), 2);
         }
     }
-    
-    // if there are no contours, just return
-    if (contourNum == -1) return;
-    
-    //polylines(img, contours, false, Scalar(0, 0, 255), 2, CV_AA);
 }
 
 void detectBall(Mat &img)
 {
+    // bounding values
     const float   hueLow = 220,   satLow = 50,    valLow = 15,
     hueHigh = 235,  satHigh = 80,   valHigh = 88;
     
@@ -83,7 +82,7 @@ void detectBall(Mat &img)
 	Mat hsv(size, CV_8U);
 	cvtColor(img, hsv, CV_BGR2HSV);
     
-    // creates mask (returned)
+    // creates B&W mask
 	Mat mask(size.height, size.width, CV_8UC1);
     
     // filters based on simple range bounds
@@ -131,8 +130,75 @@ void detectBall(Mat &img)
     float radiusEnclosing = 0;
     minEnclosingCircle(Mat(masterContour), centerEnclosing, radiusEnclosing);
     
+    // draw both the circle and the largest blob
     circle(img, centerEnclosing, round(radiusEnclosing), Scalar(0, 255, 0), 2, CV_AA);
     polylines(img, masterContour, true, Scalar(0, 255, 255), 2, CV_AA);
+}
+
+void detectBumper(Mat &img)
+{
+    // bounding values
+    const float hueLow = 220,   satLow = 50,    valLow = 15,
+                hueHigh = 245,  satHigh = 77,   valHigh = 39;
+    
+    // converts img to HSV colors
+	Size size(img.cols, img.rows);
+	Mat hsv(size, CV_8U);
+	cvtColor(img, hsv, CV_BGR2HSV);
+    
+    // creates B&W mask
+	Mat mask(size.height, size.width, CV_8UC1);
+    
+    // filters based on simple range bounds
+    inRange(hsv,
+            Scalar(kH * hueLow,     kS * satLow,    kV * valLow),
+            Scalar(kH * hueHigh,    kS * satHigh,   kV * valHigh),
+            mask);
+    
+    // stuff to shrink then grow the image
+    Point anchor1(10, 10), anchor2(5, 5);
+    Size size1(21, 21), size2(11, 11);
+    
+    Mat se21 = getStructuringElement(CV_SHAPE_RECT, size1, anchor1);
+    Mat se11 = getStructuringElement(CV_SHAPE_RECT, size2, anchor2);
+	close(mask, mask, se21);
+	open(mask, mask, se11);
+    
+    // blur image
+    GaussianBlur(mask, mask, Size(15, 15), 2, 2); // try 2,2 for last
+    
+    // finds contours of all blobs
+    vector<vector<Point>> contours;
+    findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+    
+    // finds max area of any contour and gives its position
+    int contourNum = -1;
+    double area = 0;
+    for (int i = 0; i < contours.size(); ++i) {
+        double a = contourArea(contours[i]);
+        if (a > area) contourNum = i;
+    }
+    
+    // if no contours, return
+    if (contourNum == -1) return;
+    
+    
+    // DANNYIDEA: do expensive ball find, then track the ball
+    
+    
+    // grab the biggest contour
+    vector<Point> masterContour(contours[contourNum]);
+    
+    polylines(img, masterContour, false, Scalar(0, 0, 255), 1, CV_AA);
+    
+    RotatedRect rect(minAreaRect(masterContour));
+    Point2f vertices[4];
+    rect.points(vertices);
+    
+    circle(img, rect.center, 3, Scalar(0, 255, 0), 1, CV_AA);
+    for (int k = 0; k < 4; ++k) {
+        line(img, vertices[k], vertices[(k+1) % 4], Scalar(0, 255, 255), 2);
+    }
 }
 
 void test_hot()
@@ -144,6 +210,7 @@ void test_hot()
         cam.read(img);
         detectHot(img);
         
+        // reverse direction so movement matches more naturally
         flip(img, img, 1);
         imshow("img", img);
             
@@ -161,6 +228,7 @@ void test_ball()
         cam.read(img);
         detectBall(img);
         
+        // reverse direction so movement matches more naturally
         flip(img, img, 1);
         imshow("img", img);
         
@@ -169,13 +237,32 @@ void test_ball()
 	}
 }
 
+void test_bumper()
+{
+    VideoCapture cam(0);
+    
+    for (;;) {
+        Mat img;
+        cam.read(img);
+        detectBumper(img);
+        
+        // reverse direction so movement matches more naturally
+        flip(img, img, 1);
+        imshow("img", img);
+        
+        if (waitKey(10) == 27)
+            return;
+    }
+}
+
 int main()
 {
     namedWindow("out");
     namedWindow("img");
     
-    test_hot();
+    //test_hot();
     //test_ball();
+    test_bumper();
     
 	return 0;
 }
